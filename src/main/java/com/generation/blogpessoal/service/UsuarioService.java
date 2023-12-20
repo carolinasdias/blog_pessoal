b443c919819831700f1ab9d684864450c5541f40
Carolina <carolinasdias06@gmail.com>
2023-12-20 16:14:00 -0300

    corrigindo atualizar usuario

--- src/main/java/com/generation/blogpessoal/service/UsuarioService.java
+++ src/main/java/com/generation/blogpessoal/service/UsuarioService.java
@@ -3,11 +3,13 @@ package com.generation.blogpessoal.service;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.blogpessoal.model.Usuario;
 import com.generation.blogpessoal.model.UsuarioLogin;
@@ -35,17 +37,25 @@ public class UsuarioService {
 		return Optional.ofNullable(usuarioRepository.save(usuario));
 	}
 	
-	public Optional<Usuario> atualizarUsuario(Usuario usuario){
-		if(usuarioRepository.findByUsuario(usuario.getUsuario()).isPresent()) {
-			
-			usuario.setSenha(criptografarSenha(usuario.getSenha()));
-			
-			return Optional.ofNullable(usuarioRepository.save(usuario));
-		}
-					
+public Optional<Usuario> atualizarUsuario(Usuario usuario) {
 		
+		if(usuarioRepository.findById(usuario.getId()).isPresent()) {
+
+			Optional<Usuario> buscaUsuario = usuarioRepository.findByUsuario(usuario.getUsuario());
+
+			if ( (buscaUsuario.isPresent()) && ( buscaUsuario.get().getId() != usuario.getId()))
+				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Usuário já existe!", null);
+
+			usuario.setSenha(criptografarSenha(usuario.getSenha()));
+
+			return Optional.ofNullable(usuarioRepository.save(usuario));
+			
+		}
+
 		return Optional.empty();
-	}
+	
+	}	
+
 
 	public Optional<UsuarioLogin> autenticarUsuario(Optional<UsuarioLogin> usuarioLogin){
 		var credenciais = new UsernamePasswordAuthenticationToken(usuarioLogin.get().getUsuario(), usuarioLogin.get().getSenha());
